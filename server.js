39c187dd1e52b71cc3ffd7a5c6f92305fd6028a2
yossiD <dagangm@gmail.com>
2019-05-12 12:00:56 +0300

    basic setup

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const bodyParser = require('body-parser')
+const api = require('./server/routes/api')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bankDB', { useNewUrlParser: true })
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+    next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/', api)
+
+
+
+
+
+app.get('/sanity', function (req, res) {
+    res.send("ok")
+})
+
+const port = 4500
+app.listen(port, function () {
+    console.log(`Server running on ${port}`)
+})
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { directive } from '@babel/types';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>Hi</div>
+    )
+  }
+}
+
+export default App;
