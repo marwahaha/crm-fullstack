2b6b1f55e03736115fe1f21c1229372305d34dfa
yossiD <dagangm@gmail.com>
2019-05-14 21:37:03 +0300

    month renders dynamically

--- src/components/analytics/NewClientsBadge.js
+++ src/components/analytics/NewClientsBadge.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 
 class NewClientsBadge extends Component {
 
+    
+    getMonthName = (date = new Date()) => new Intl.DateTimeFormat('en-US', {month: "long"}).format(date)
 
-    getNewClients = () => {
+    getNewestClients = () => {
         let newClients = this.props.clientsData.filter(c =>
             (new Date(c.firstContact).getMonth() == new Date().getMonth()) &&
             (new Date(c.firstContact).getFullYear() == new Date().getFullYear())).length
         return newClients
     }
 
+
+
     render() {
+        console.log(new Date().getMonth())
         return (
             <div id="badge">
                 <span id="logoNewest" className="fas fa-chart-line"></span>
-                <span id="value">{this.getNewClients()}</span>
-                <span id="text">New September Clients</span>
+                <span id="value">{this.getNewestClients()}</span>
+                <span id="text">New {this.getMonthName()} Clients</span>
             </div>
         );
     }
