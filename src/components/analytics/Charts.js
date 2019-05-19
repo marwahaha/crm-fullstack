7b6d4af09d58a1bf792b0385b9e49e82cde545d1
yossiD <dagangm@gmail.com>
2019-05-19 17:07:39 +0300

    added sales by country

--- src/components/analytics/Charts.js
+++ src/components/analytics/Charts.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import TopEmployees from './charts/TopEmployees';
 import SalesByX from './charts/SalesByX';
+import "../../css/charts.css"
 
 
 class Charts extends Component {
     render() {
 
         return (
-            <div>
+            <div id="charts">
                 <TopEmployees clientsData={this.props.clientsData} />
                 <SalesByX clientsData={this.props.clientsData} />
             </div>
--- src/components/analytics/charts/SalesByX.js
+++ src/components/analytics/charts/SalesByX.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import "../../../css/salesByX.css"
 
 class SalesByX extends Component {
+    constructor() {
+        super()
+        this.state = {
+            selectedCountry: "country"
+        }
+    }
+
+    changeValue = event => this.setState({ [event.target.name]: event.target.value })
+    getFullMonth = (date = new Date()) => new Intl.DateTimeFormat('en-US', { month: "long" }).format(date).slice(0, 3)
+
+    getDataSet = (selection) => {
+        let AllData = new Set()
+        selection == "month" ?
+            this.props.clientsData.forEach(c => AllData.add(this.getFullMonth(new Date(c.firstContact)))) :
+            this.props.clientsData.forEach(c => c[selection] ? AllData.add(c[selection]) : null)
+        return Array.from(AllData)
+    }
+
+    generateData = (selection) => {
+        let AllData = this.getDataSet(selection)
+        let dataArray = []
+        AllData.forEach(d => dataArray.push({ name: d, sales: 0 }))
+        for (let c of this.props.clientsData) {
+            for (let i in dataArray) {
+                if (dataArray[i].name === c[selection]) {
+                    dataArray[i].sales++
+                } else if (dataArray[i].name === this.getFullMonth(new Date(c.firstContact))) {
+                    dataArray[i].sales++
+                }
+            }
+        }
+        return dataArray
+    }
+
+
     render() {
+        let data = this.generateData(this.state.selectedCountry)
+
         return (
-            <div>
-                
+            <div id="salesByCountryChart">
+                <div className="title">Sales By
+                    <select name="select" id="" onChange={this.changeValue}>
+                        <option value="country">Country</option>
+                        <option value="owner">Owner</option>
+                        <option value="emailType">Email Type</option>
+                        <option value="month">Month</option>
+                    </select>
+                </div>
+                <ResponsiveContainer width="100%" height="90%">
+
+                    <BarChart
+                        data={data}
+                        margin={{
+                            top: 5, right: 30, left: 20, bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis dataKey="sales" />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="sales" fill="green" />
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         );
     }
